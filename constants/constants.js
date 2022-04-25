2176398cff76029c0c9ecaeb99d807bdc2f09e96
Jeff Elsey <jcelsey@gmail.com>
2022-04-25 15:59:22 -0600

    initial ico-tutorial commit

---
+++ constants/constants.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xb8c6d952Ac8110B57a53d3b5Cb22240D022CF5E4";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
